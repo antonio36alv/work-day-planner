ea112997cae4ea646605af79a6ea04e26ba4a47f
Antonio Alvarado <ant.36alv@gmail.com>
2020-02-14 20:52:40 -0500

    Application is finished. Works great.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # work-day-planner
+
+[Link to the launched application](https://antonio36alv.github.io/work-day-planner/)
+[Link to the Repository](https://github.com/antonio36alv/work-day-planner)
+
+## Project Description
+
+### Link video here
--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-$("document").ready(function() {
-    //TODO delete line under
-    // localStorage.clear();
+$("document").ready(function () {
     var container = document.querySelector(".container");
-    //TODO when the page is open display the current day at the top of the calendaar
-    
+    var displayCurrentDay = document.getElementById("currentDay");
+    displayCurrentDay.textContent = moment().format("dddd");
+
     //initilize an array responsible for timeblocks, will either be empty or will
     //come from previous local storage
     var timeblocks = JSON.parse(localStorage.getItem("timeblocks") || "[]");
-    console.log(timeblocks);
 
     class timeblock {
         //https://www.youtube.com/watch?v=h33Srr5J9nY
-        constructor(hour, eventLog){
+        constructor(hour, eventLog) {
             this.hour = hour;
             this.eventLog = eventLog;
             this.condense();
@@ -19,124 +17,136 @@ $("document").ready(function() {
         //function condenses timeblocks array. if a previous timeblock represents the same hour
         //will take old eventlog and add it to this one. then splice the old array.
         condense = () => {
-                //go through timeblocks array
-                for(i = 0; i < timeblocks.length; i++) {
-                    //if this objects hour matches another objects hour
-                    if(this.hour==timeblocks[i].hour) {
-                        //set our eventlog to the end of the previous logs.
-                        this.eventLog = timeblocks[i].eventLog.concat(this.eventLog);
-                        //get rid of the previous array
-                        timeblocks.splice(i, 1);
-                    }
+            //go through timeblocks array
+            for (i = 0; i < timeblocks.length; i++) {
+                //if this objects hour matches another objects hour
+                if (this.hour == timeblocks[i].hour) {
+                    timeblocks.splice(i, 1);
                 }
+            }
         }
-        //TODO think of creating a function
-        //for returning or controling the color indicator (past present future)
     }
-    //will be color coded to indicate whether that timeblock is in the past, in the current hour, or the future
+    //generate timeblocks
+    generateTimeblocks();
+    //add event listeners to save buttons and tie a function to them
+    saveButtonsAddEvents();
+    //initial update, update is also called within the save buttons event handlers
+    update();
 
-    //each loop building one div that contains the following
-    // <div>     <input>    <button>
-    //hour div,event input, save btn.
-    for(i = 1; i < 25; i++) {
-        //div for the whole timeblock
-        let displayBlock = document.createElement("div");
-        displayBlock.setAttribute("class", "timeblock");
-        //make a div for hour, edit text, save button
-        let displayHour = document.createElement("div");
-        let p = document.createElement("p");
-        displayHour.appendChild(p);
-        displayHour.setAttribute("class", "displayHour");
-        //make a input for event
-        let displayInput = document.createElement("textarea");
-        displayInput.setAttribute("class", "displayInput");
-        displayInput.setAttribute("id", ("displayInput" + i))
-        //make a button for saving the event
-        let displayBtn = document.createElement("button");
-        displayBtn.setAttribute("class", "displayBtn");
-        displayBtn.setAttribute("value", ("displayBtn" + i))
-        displayBtn.textContent = "Saved";
-        //append hour div, event input, and button to the timeblock
-        displayBlock.appendChild(displayHour);
-        displayBlock.appendChild(displayInput);
-        displayBlock.appendChild(displayBtn);
+    function generateTimeblocks() {
+        //each loop building one div that contains the following
+        // <div>     <input>    <button>
+        //hour div,event input, save btn.
+        for (i = 1; i < 25; i++) {
+            //div for the whole timeblock
+            let displayBlock = document.createElement("div");
+            displayBlock.setAttribute("class", "timeblock");
+            //make a div for hour, edit text, save button
+            let displayHour = document.createElement("div");
+            let p = document.createElement("p");
+            displayHour.appendChild(p);
+            displayHour.setAttribute("class", "displayHour");
+            //make a input for event
+            let displayInput = document.createElement("textarea");
+            displayInput.setAttribute("class", "displayInput");
+            displayInput.setAttribute("id", ("displayInput" + i))
+            //make a button for saving the event
+            let displayBtn = document.createElement("button");
+            displayBtn.setAttribute("class", "displayBtn");
+            displayBtn.setAttribute("value", ("displayBtn" + i))
+            displayBtn.textContent = "Saved";
+            //append hour div, event input, and button to the timeblock
+            displayBlock.appendChild(displayHour);
+            displayBlock.appendChild(displayInput);
+            displayBlock.appendChild(displayBtn);
 
-        //if greater than 12, start at 1 again and print pm
-        if(i > 12) {
-            p.textContent = (i - 12) + "pm";
-        } else {
-            //under 12 print am
-            p.textContent = i + "am";
+            //if greater than 12, start at 1 again and print pm
+            if (i > 12) {
+                p.textContent = (i - 12) + "pm";
+            } else {
+                //under 12 print am
+                p.textContent = i + "am";
+            }
+            //timeblock is appeneded to container
+            container.appendChild(displayBlock);
         }
-        //timeblock is appeneded to container
-        container.appendChild(displayBlock);
     }
     //when saved it is saved to the local storage
     //when the page is refreshed the local storage will have the events load up
-
-    function update(){
+    function update() {
         //we can either have this function have the if statement determine to loop through
-
+        localStorage.setItem("timeblocks", JSON.stringify(timeblocks));
         //figure out which one is the current our, everything before that is now grayed out - past,
         //everything after...
 
         //index 0 is 1 so 
         //time = currentHour + 1;
         //eg you want 1am, since it is the first hour, 0 will get it
-        let currentHour = 0;
-        console.log(container.children[0]);
-        
+        let currentHour = moment().format("HH") - 1;
+
         //up untill the current hour we will change the divs to our past color
-        for(i = 0; i < currentHour; i++) {
-            let div = container.children[i];
-            div.style.backgroundColor = "gray";
-            div.style.color = "gray";
-            div.style.border = "gray solid thick";
+        for (i = 0; i < currentHour; i++) {
+            let divPast = container.children[i];
+            divPast.style.backgroundColor = "gray";
+            divPast.style.border = "gray solid thick";
+            container.children[i].children[2].style.backgroundColor = "gray";
         }
-        let present = container.children[currentHour];
-        present.style.backgroundColor = "red";
-        present.style.color = "red";
-        present.style.border = "green solid thick";
 
-        for(i = 0; i < timeblocks.length; i++) {
+        let divPresent = container.children[currentHour];
+        divPresent.style.backgroundColor = "green";
+        divPresent.style.border = "green solid thick";
+        container.children[currentHour].children[2].style.backgroundColor = "green";
+
+        for (i = currentHour + 1; i < 24; i++) {
+            let divFuture = container.children[i];
+            divFuture.style.backgroundColor = "blue";
+            divFuture.style.border = "blue solid thick";
+            container.children[i].children[2].style.backgroundColor = "blue";
+        }
+
+        for (i = 0; i < timeblocks.length; i++) {
             //to update our logs, we must loop through the saved logs and get that eventlog
-            
             //we have to set the appropiate displayinputs to those event logs
-
             let hour = timeblocks[i].hour - 1;
-            console.log(hour);
             container.children[hour].children[1].textContent = timeblocks[i].eventLog;
         }
-        // console.log(container.children[currentHour].style.color = "red");
-        // for(i = 0; container.children[i])
     }
-    update();
-    //TODO complete save button event listener
-    //TODO add update displayInput text
-    //loop through the containers children, which are the divs for timeblocks
-    for(i = 0; i < container.childElementCount; i++) {
-        //their second element child is the save button
-        container.children[i].children[2].addEventListener("click", () => {
-            //https://www.youtube.com/watch?v=h33Srr5J9nY
 
-            //TODO see if i can condense this
-            let value = event.target.value;
-            let instance = value.charAt(value.length - 1);
-            let textInput = document.querySelector(("#displayInput" + instance));
-            let eventLog = textInput.value;
-            if(eventLog===""){
-                //TODO user did not enter any text for the event, be assertive tell them so
-            } else {
-                timeblocks.push(tb = new timeblock(instance, eventLog));
-                localStorage.setItem("timeblocks", JSON.stringify(timeblocks));
-                // tb.condense();
+    function saveButtonsAddEvents() {
+        //loop through the containers children, which are the divs for timeblocks
+        for (i = 0; i < container.childElementCount; i++) {
+            //their second element child is the save button
+            container.children[i].children[2].addEventListener("click", () => {
+                //https://www.youtube.com/watch?v=h33Srr5J9nY
+
+                //TODO see if i can condense this
+                let value = event.target.value;
+                let instance = value.substring(10);
+                let textInput = document.querySelector(("#displayInput" + instance));
+                let eventLog = textInput.value;
+                if (eventLog !== "") {
+                    //create timeblock instance with the hour being logged and the event
+                    timeblocks.push(new timeblock(instance, eventLog));
+                    update();
+                } else {
+                    if(findTimeblock(instance) > 0) {
+                        let matchIndex = findTimeblock(instance);
+                        timeblocks.push(new timeblock(matchIndex, eventLog))
+                    }
+                    update();
+                }
+            });
+        }
+    }
+    //find out if there is a timeblock already stored
+    //that matches the hour we pass in
+    function findTimeblock(hour) {
+        for (i = 0; i < timeblocks.length; i++) {
+            if (timeblocks[i].hour == hour) {
+                //found it so return it
+                return timeblocks[i].hour;
             }
-        });
-        //each input has the an value of  of displayInputX
-        //each save button has the an value of  of displayBtnX
-
-        //user clicks the save button
-        //check to see that there is text in displayInputX, we determine which X  based on the value of the event
-        //save that text and the X
-    }    
+        }
+        return -1;
+    }
 });
